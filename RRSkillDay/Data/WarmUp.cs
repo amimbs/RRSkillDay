9f55a104ac6ba20df4b86f202eb15e7206c56bce
amimbs <andymimbs91@gmail.com>
2023-01-08 12:01:32 -0500

    feat: add class and service for warmups

---
+++ RRSkillDay/Data/WarmUp.cs
@@ -0,0 +1,10 @@
+﻿namespace RRSkillDay.Data
+{
+	public class WarmUp
+	{
+		public string Title { get; set; }
+		public string Repetitions { get; set; }
+		public string Link { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ RRSkillDay/Data/WarmUpService.cs
@@ -0,0 +1,34 @@
+﻿namespace RRSkillDay.Data
+{
+	public class WarmUpService
+	{
+		public List<WarmUp> WarmUps = new();
+		public WarmUpService() 
+		{ 
+			WarmUps.Add(new WarmUp()
+			{
+				Title = "hell",
+				Repetitions = "1",
+				Link = "safsd",
+				Description = "Description",
+			});
+
+			WarmUps.Add(new WarmUp()
+			{
+				Title = "hell",
+				Repetitions = "1",
+				Link = "safsd",
+				Description = "Description",
+			});
+
+			WarmUps.Add(new WarmUp()
+			{
+				Title = "hell",
+				Repetitions = "1",
+				Link = "safsd",
+				Description = "Description",
+			});
+
+		}
+	}
+}
