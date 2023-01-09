43de70ab55720757e8ef39621b25eb483ff5b4a6
amimbs <andymimbs91@gmail.com>
2023-01-09 01:11:17 -0500

    bug: compiler error, warmup page is not receiving class definitions

--- RRSkillDay/Data/WarmUp.cs
+++ RRSkillDay/Data/WarmUp.cs
@@ -4,8 +4,10 @@
 	{
 		public string Title { get; set; }
 		public string Repetitions { get; set; }
-		public string Link { get; set; }
+		public string ImageUrl { get; set; }
 		public string Description { get; set; }
+		public string VideoUrl { get; set; }
+
 	}
 }
 
--- RRSkillDay/Data/WarmUpService.cs
+++ RRSkillDay/Data/WarmUpService.cs
@@ -1,63 +1,37 @@
 ﻿namespace RRSkillDay.Data
 {
-	//public class WarmUpService
-	//{
-	//	public List<WarmUp> WarmUps = new();
-	//	public WarmUpService() 
-	//	{ 
-	//		WarmUps.Add(new WarmUp()
-	//		{
-	//			Title = "hell",
-	//			Repetitions = "1",
-	//			Link = "safsd",
-	//			Description = "Description",
-	//		});
-
-	//		WarmUps.Add(new WarmUp()
-	//		{
-	//			Title = "hell",
-	//			Repetitions = "1",
-	//			Link = "safsd",
-	//			Description = "Description",
-	//		});
-
-	//		WarmUps.Add(new WarmUp()
-	//		{
-	//			Title = "hell",
-	//			Repetitions = "1",
-	//			Link = "safsd",
-	//			Description = "Description",
-	//		});
-
-	//	}
-	//}
-
-	public class WarmUpService
-	{
-		public List<WarmUp> GetWarmUps = new()
-		{
-			new WarmUp
-			{
-				Title = "cat",
-				Repetitions = "1",
-				Link = "safsd",
-				Description = "Description",
-			},
-			new WarmUp
-			{
-				Title = "hell",
-				Repetitions = "1",
-				Link = "safsd",
-				Description = "Description",
-			},
-			new WarmUp
-			{
-				Title = "dog",
-				Repetitions = "1",
-				Link = "safsd",
-				Description = "Description",
-			},
-
-		};
-	}
+    public class WarmUpService
+    {
+        public List<WarmUp> GetWarmUps()
+        {
+            var warmUps = new List<WarmUp>
+            {
+                new WarmUp
+                {
+                    Title = "cat",
+                    Repetitions = "1",
+                    ImageUrl = "safsd",
+                    Description = "Description",
+                    VideoUrl = "utubz"
+                },
+                new WarmUp
+                {
+                    Title = "dog",
+                    Repetitions = "2",
+                    ImageUrl = "safsd",
+                    Description = "Description",
+                    VideoUrl = "utubz"
+                },
+                new WarmUp
+                {
+                    Title = "rat",
+                    Repetitions = "3",
+                    ImageUrl = "safsd",
+                    Description = "Description",
+                    VideoUrl = "utubz"
+                },
+            };
+            return warmUps;
+        }
+    }
 }
