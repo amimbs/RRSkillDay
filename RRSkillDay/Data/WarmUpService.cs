95436125efa647c5f8c48ae8587d77883f5fd895
amimbs <andymimbs91@gmail.com>
2023-01-09 00:08:49 -0500

    feat: add new Models folder, begin refactoring warmup service and cs files

--- RRSkillDay/Data/WarmUpService.cs
+++ RRSkillDay/Data/WarmUpService.cs
@@ -1,34 +1,63 @@
 ﻿namespace RRSkillDay.Data
 {
+	//public class WarmUpService
+	//{
+	//	public List<WarmUp> WarmUps = new();
+	//	public WarmUpService() 
+	//	{ 
+	//		WarmUps.Add(new WarmUp()
+	//		{
+	//			Title = "hell",
+	//			Repetitions = "1",
+	//			Link = "safsd",
+	//			Description = "Description",
+	//		});
+
+	//		WarmUps.Add(new WarmUp()
+	//		{
+	//			Title = "hell",
+	//			Repetitions = "1",
+	//			Link = "safsd",
+	//			Description = "Description",
+	//		});
+
+	//		WarmUps.Add(new WarmUp()
+	//		{
+	//			Title = "hell",
+	//			Repetitions = "1",
+	//			Link = "safsd",
+	//			Description = "Description",
+	//		});
+
+	//	}
+	//}
+
 	public class WarmUpService
 	{
-		public List<WarmUp> WarmUps = new();
-		public WarmUpService() 
-		{ 
-			WarmUps.Add(new WarmUp()
+		public List<WarmUp> GetWarmUps = new()
+		{
+			new WarmUp
+			{
+				Title = "cat",
+				Repetitions = "1",
+				Link = "safsd",
+				Description = "Description",
+			},
+			new WarmUp
 			{
 				Title = "hell",
 				Repetitions = "1",
 				Link = "safsd",
 				Description = "Description",
-			});
-
-			WarmUps.Add(new WarmUp()
+			},
+			new WarmUp
 			{
-				Title = "hell",
+				Title = "dog",
 				Repetitions = "1",
 				Link = "safsd",
 				Description = "Description",
-			});
+			},
 
-			WarmUps.Add(new WarmUp()
-			{
-				Title = "hell",
-				Repetitions = "1",
-				Link = "safsd",
-				Description = "Description",
-			});
-
-		}
+		};
 	}
 }
